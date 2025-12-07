a3030165d09fad2265637e8aa80ceafab7a728a2
lhmuana <edd_vai@yahoo.co.in>
2025-12-07 16:09:35 +0530

    image files added with the Roll button

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,26 +1,42 @@
-
 import 'package:final_dice_roll/styled_text.dart';
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget {
-
-  const GradientContainer(this.color1, this.color2,{super.key});
+  const GradientContainer(this.color1, this.color2, {super.key});
   final Color color1;
   final Color color2;
+
+  void rollDice() {
+    //empty
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [color1,color2,],
-              begin: Alignment.bottomLeft,
-              end: Alignment.topRight,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/images/dice-3.png', width: 200),
+            const SizedBox(height: 30),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                textStyle: TextStyle(fontSize: 25),
+              ),
+              child: const Text('Data'),
             ),
-          ),
-          child: Center(
-            child: StyledText('Final dice-app'),
-          ),
-        );
+          ],
+        ),
+      ),
+    );
   }
 }
